8dee27404f5577e564351243da6e50079f2acfe6
https-Anamika <https-Anamika@github.com>
2025-03-30 18:57:23 +0530

    Initial Commit

---
+++ clima/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  clima
+//
+//  Created by anamika singh on 12/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var conditionImage: UIImageView!
+    
+    @IBOutlet weak var temperaturLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func searchPresses(_ sender: UIButton) {
+      
+    }
+}
+
